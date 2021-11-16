4e8edd5d769b72b710c61fbd87bdd1a4587dbf4f
Dennis Frankenbach <dennisfrankenbach@gmail.com>
2021-11-16 22:51:35 +0100

    feat: add camera permissions

--- app/src/main/java/net/fabiszewski/ulogger/QRFragment.java
+++ app/src/main/java/net/fabiszewski/ulogger/QRFragment.java
@@ -27,6 +27,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,9 +35,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.concurrent.ExecutorService;
@@ -49,7 +52,18 @@ import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
 import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
 import static java.util.concurrent.Executors.newCachedThreadPool;
 
+import com.budiyev.android.codescanner.AutoFocusMode;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.ScanMode;
+
 public class QRFragment extends Fragment implements LoggerTask.LoggerTaskCallback, ImageTask.ImageTaskCallback {
+
+    private TextView locationNotFoundTextView;
+    private CodeScanner codeScanner;
+    private View generalView;
+
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
 
     public QRFragment() {
     }
@@ -81,6 +95,64 @@ public class QRFragment extends Fragment implements LoggerTask.LoggerTaskCallbac
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions( //Method of Fragment
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_CAMERA_REQUEST_CODE
+            );
+        } else {
+            Log.e("CAMERA", "PERMISSION GRANTED");
+        }
         return inflater.inflate(R.layout.fragment_scan_barcode_from_camera, container, false);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (permissions[0].equals(Manifest.permission.CAMERA)
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.e("CAMERA","camera permission granted");
+            } else {
+                Log.e("CAMERA", "camera permission denied");
+            }
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        generalView = view;
+        initScanner();
+        initFlashButton(view);
+
+    }
+
+
+    private void initScanner(){
+        CodeScannerView scannerView = generalView.findViewById(R.id.scanner_view);
+        codeScanner = new CodeScanner(generalView.getContext(), scannerView);
+        codeScanner.setCamera(CodeScanner.CAMERA_BACK);
+        codeScanner.setAutoFocusMode(AutoFocusMode.CONTINUOUS);
+        codeScanner.setFormats(CodeScanner.ALL_FORMATS);
+        codeScanner.setScanMode(ScanMode.SINGLE);
+        codeScanner.setAutoFocusEnabled(true);
+        //codeScanner.isFlashEnabled()//todo:add
+        codeScanner.setTouchFocusEnabled(false);
+        //codeScanner.setDecodeCallback(this::handleScannedBarcode); //todo: add
+        //codeScanner.setErrorCallback(this::showError); //todo: add
+    }
+
+    private void initFlashButton(View view) {
+
+        FrameLayout flash_container = view.findViewById(R.id.layout_flash_container);
+        flash_container.setOnClickListener(this::toggleFlash);
+
+    }
+
+    private void toggleFlash(View view){
+        ImageView flash = generalView.findViewById(R.id.image_view_flash);
+        flash.setActivated(!flash.isActivated());
+        codeScanner.setFlashEnabled(!codeScanner.isFlashEnabled());
+    }
+
 }
